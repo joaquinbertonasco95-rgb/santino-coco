cb046fe19e8540d3fb7536bdace20930b5b9e3c9
Tu Nombre <joaquinbertonasco95@gmail.com>
2025-11-06 14:10:19 -0300

    santinono

---
+++ main.js
@@ -0,0 +1,33 @@
+// MAIN 1 FUNCION NORMAL PARA UN SOLO NUMERO DE TELEFONO
+window.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector(".Btn");
+    setTimeout(() => {
+        btn.classList.add("expand");
+    }, 300); // delay para suavizar
+});
+
+// evento de click para el pixel
+
+document.addEventListener("DOMContentLoaded", () => {
+  const linkBtn = document.getElementById("linkBtn");
+
+  if (linkBtn) {
+    linkBtn.addEventListener("click", (event) => {
+      fbq('trackCustom', 'LinkClick', { url: linkBtn.href });
+      console.log("Evento Pixel: clic en link detectado →", linkBtn.href);
+    });
+  }
+});
+
+
+const contactNumber = "5493516069761"; // Reemplaza con tu numero (con código país sin + y sin espacios)
+const message = "Hola, quiero un usuario."; // reemplaza el mensaje dentro de las (""), no las borres
+
+function sendWhatsAppMessage() {
+    const encodedMessage = encodeURIComponent(message);
+    const url = `https://wa.me/${contactNumber}?text=${encodedMessage}`;
+    window.open(url, "_blank");
+}
+
+// Vincular al botón
+document.querySelector(".Btn").addEventListener("click", sendWhatsAppMessage);
---
+++ readme.txt
@@ -0,0 +1,20 @@
+-- Instalás Git. --
+
+// Configurás tu nombre y mail (para que quede en los commits): //
+
+git config --global user.name "Tu Nombre"
+git config --global user.email "joaquinbertonasco95@gmail.com"
+
+
+Cuando hacés tu primer git push origin main, se abre una ventanita de GitHub → iniciás sesión y listo.
+🔒 Git Credential Manager guarda la sesión en tu PC, no tenés que volver a loguearte.
+
+Después ya es:
+
+git init
+
+git add .
+
+git commit -m "mensaje"
+
+git push
