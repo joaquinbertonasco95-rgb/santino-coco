cefc7671d37bf409faab06011dc9a6d89c6d9ad5
Tu Nombre <joaquinbertonasco95@gmail.com>
2025-12-01 01:52:15 -0300

    mkjasd

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-const contactNumber = "5493513220244"; // Reemplaza con tu numero (con código país sin + y sin espacios)
+const contactNumber = "5493515749410"; // Reemplaza con tu numero (con código país sin + y sin espacios)
 const message = "Hola, quiero un usuario."; // reemplaza el mensaje dentro de las (""), no las borres
 
 function sendWhatsAppMessage() {
